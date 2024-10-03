21ae36973f3289c84c7f1c9bbd0e946e009b7d8d
TobiasNethan <sbttobias@gmail.com>
2024-10-03 10:25:21 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Function expression that divides two numbers
+const divide = (a, b) => a / b;
+
+// Arrow function that squares a number
+const square = x => x * x;
+
+// Arrow function that adds two numbers
+const add = (a, b) => a + b;
